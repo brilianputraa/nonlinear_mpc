93cfdf3e7aa21f0f504e044b0d51f65aa39c943e
Brilian Putra Amiruddin <brilianpamiruddin@gmail.com>
2023-11-24 23:48:55 +0900

    Update BatchSim.py
    Fixing some issues with conditional logic

--- src/mpc_local_planner/behavior_manager/scripts/BatchSim.py
+++ src/mpc_local_planner/behavior_manager/scripts/BatchSim.py
@@ -48,6 +48,8 @@ class BatchSim:
         self.goal_status = None
         self.goal_reached = False
         self.status = None
+        self.timeout = False
+        self.time = None
         # Helper classes
         self.gh = gh.GazeboHelper()
         self.gcg = gcg.GoalCheckerandGetter()
@@ -58,7 +60,7 @@ class BatchSim:
         self.pickup_point_list_dir = rospy.get_param("~pickup_point_list_dir")
         self.mpc_local_planner_launch_dir = rospy.get_param("~mpc_without_global_planner_dir")
         # ROS Subscribers
-        self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
+        self.odom_sub = rospy.Subscriber("/INS/odom", Odometry, self.odom_callback)
         self.cmd_vel_sub = rospy.Subscriber("/cmd_vel", Twist, self.cmd_vel_callback)
         # Get the goal pose of the robot in the gazebo world
         self.pickup_point_list = np.loadtxt(self.pickup_point_list_dir, delimiter=',')
@@ -67,14 +69,14 @@ class BatchSim:
         self.grid_x_max = 29.071
         self.grid_y_min = -1.609
         self.grid_y_max = 1.391
-        self.grid_yaw_min = 0.0
-        self.grid_yaw_max = np.pi/4
+        self.grid_yaw_max = 0.0
+        self.grid_yaw_min = -np.pi/4
         # Incrementing grid size for the grid search
         self.increment_grid_size_x = 0.2
         self.increment_grid_size_y = 0.2
         self.increment_grid_size_yaw = 0.0872665  
         # ROS Timeouts
-        self.move_base_client_timeout = 60.0
+        self.move_base_client_timeout = 50.0
         # Loggers
         self.current_date_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         self.logdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "logs", self.current_date_time)
@@ -85,7 +87,7 @@ class BatchSim:
     def run_batch_sim(self):
         os.mkdir(self.logdir)
         rospy.sleep(0.1)
-        with open(os.path.join(self.logdir, "log_everything.txt"), "w") as self.log_file_everything:
+        with open(os.path.join(self.logdir, "log_all_grid.txt"), "w") as self.log_file_everything:
             # Write Header to the log file
             # print("WRITING HEADER")
             self.log_file_everything.write("x_grid,y_grid,yaw_grid,distance_travelled,time_elapsed,goal_status\n")
@@ -95,8 +97,6 @@ class BatchSim:
                 for y in np.arange(self.grid_y_min, self.grid_y_max, self.increment_grid_size_y):
                     # For each yaw in the grid area
                     for yaw in np.arange(self.grid_yaw_min, self.grid_yaw_max, self.increment_grid_size_yaw):
-                        # Write the current grid point to the log file
-                        self.log_file_everything.write(str(x) + "," + str(y) + "," + str(yaw) + ",")
                         # Set the log file name for the current grid point
                         self.log_file_name_grid_point = "log_per_grid_point_" + str(x) + "_" + str(y) + "_" + str(yaw) + ".txt"
                         # Open the log file for the current grid point
@@ -106,6 +106,7 @@ class BatchSim:
                             self.log_file_per_grid_point.write("time,current_pose_x,current_pose_y,current_pose_heading,current_twist_linear_x,current_twist_linear_y,current_twist_angular_x,current_twist_angular_y,current_twist_angular_z,current_vel_command,current_steering_command,goal_status\n")
                             # Set the pose of the robot to the current grid poin
                             self.gh.setModelPoseAndSpeed("rbcar", [x, y, self.pose_z, 0.0, 0.0, yaw], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+                            rospy.sleep(0.1)
                             # Launch the mpc_local_planner
                             uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
                             roslaunch.configure_logging(uuid)
@@ -119,28 +120,32 @@ class BatchSim:
                             self.client.send_goal(goal)
                             self.time = rospy.Time.now()
                             # Wait for the goal to be reached
-                            while self.status != actionlib.GoalStatus.SUCCEEDED or self.status != actionlib.GoalStatus.ABORTED or not self.timeout:
+                            while self.status != 3 and self.status != 4 and not self.timeout:
                                 self.status = self.client.get_state()
-                                self.timeout = (rospy.Time.now() - self.time) > rospy.Duration.from_sec(self.move_base_client_timeout)
+                                # print("Running Time: " + str(rospy.Time.now().to_sec() - self.time.to_sec()) + " Goal Status: " + str(self.status))
+                                self.timeout = (rospy.Time.now().to_sec() - self.time.to_sec()) > self.move_base_client_timeout
                                 # If new pose is received then write to the log file
                                 if self.odom_stamp != self.previous_odom_stamp:
+                                    # print(4)
                                     self.log_file_per_grid_point.write(str(rospy.Time.now()) + "," + str(self.current_pose_x) + "," + str(self.current_pose_y) + "," + str(self.current_pose_heading) + "," + str(self.current_twist_linear_x) + "," + str(self.current_twist_linear_y) + "," + str(self.current_twist_angular_x) + "," + str(self.current_twist_angular_y) + "," + str(self.current_twist_angular_z) + "," + str(self.current_vel_command) + "," + str(self.current_steering_command) + "," + str(self.status) + "\n")
                                     self.current_pose.append([self.current_pose_x, self.current_pose_y])
                                     self.previous_odom_stamp = self.odom_stamp
+                        # Calculate the time elapsed
+                        self.time_elapsed = rospy.Time.now().to_sec() - self.time.to_sec()
                         # Shutdown the mpc_local_planner
                         launch_without_gp.shutdown()
-                        # Calculate the time elapsed
-                        self.time_elapsed = rospy.Time.now() - self.time
                         # Calculate the distance travelled
-                        self.distance_travelled = np.linalg.norm(np.array(self.current_pose[1:, :]) - np.array(self.current_pose[:-1, :]))
-                        self.log_file_everything.write(self.distance_travelled + "," + self.time_elapsed + "," + self.status + "\n")
+                        current_pose_array = np.array(self.current_pose)
+                        self.distance_travelled = np.linalg.norm(current_pose_array[1:, :] - current_pose_array[:-1, :], axis=1).sum()
+                        print("Distance Travelled: " + str(self.distance_travelled) + " Time Elapsed: " + str(self.time_elapsed) + " Status: " + str(self.status))
                         # Write to the whole log file
-                        if self.status == actionlib.GoalStatus.SUCCEEDED:
-                            self.log_file_everything.write("SUCCEEDED" + "\n")
-                        elif self.status == actionlib.GoalStatus.ABORTED:
-                            self.log_file_everything.write("ABORTED" + "\n")
+                        if self.status == 3:
+                            self.goal_status = "SUCCEDDED"
+                        elif self.status == 4:
+                            self.goal_status = "ABORTED"
                         elif self.timeout:
-                            self.log_file_everything.write("TIMEOUT" + "\n")
+                            self.goal_status = "TIMEOUT"
+                        self.log_file_everything.write(str(x) + "," + str(y) + "," + str(yaw) + "," + str(self.distance_travelled) + "," + str(self.time_elapsed) + "," + str(self.goal_status) + "\n")
                         # Reset the variables
                         self.reset_variables()
                     
